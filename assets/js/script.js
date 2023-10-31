26eb904b1ab56e5be85b6def9638b2b7732e1ce7
johanpq <johanp.queiroz@gmail.com>
2023-10-31 11:14:19 -0300

    added script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
+var Name = document.getElementById('Name')
 
+var Age = window.document.getElementById('Age')
+
+var Gender = window.document.getElementsByName('gender')
+
+var SelectedGender
+
+for(let i = 0; i < Gender.length; i++) {
+    if(Gender[i].checked) {
+        SelectedGender = Gender[i].value
+        break
+    }
+}
+
+var Submit = document.getElementById('Submit')
+Submit.addEventListener('click', Click)
+
+function Click() {
+    let NameValue = Name.value
+    let AgeValue = Number(Age.value)
+    let GenderValue = SelectedGender
+
+     alert("Nome = " + NameValue + "Idade = " + AgeValue + "Genero = " + GenderValue) 
+}
