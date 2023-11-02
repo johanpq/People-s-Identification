ff3e4510ad3a05e4b2ade31d44e4571f558d9994
johanpq <johanp.queiroz@gmail.com>
2023-11-02 15:13:29 -0300

    Updating

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,50 @@
 var Name = document.getElementById('Name')
+var Age = document.getElementById('Age')
 
-var Age = window.document.getElementById('Age')
+var Gender = document.getElementsByName('gender')
 
-var Gender = window.document.getElementsByName('gender')
-
-var SelectedGender
-
-for(let i = 0; i < Gender.length; i++) {
-    if(Gender[i].checked) {
-        SelectedGender = Gender[i].value
-        break
-    }
-}
+var SelectedGender = ""
 
 var Submit = document.getElementById('Submit')
 Submit.addEventListener('click', Click)
 
 function Click() {
-    let NameValue = Name.value
-    let AgeValue = Number(Age.value)
-    let GenderValue = SelectedGender
+    if(Name.value.trim() === "") {
+        alert("Type your name please! ")
+        //checa se o nome não recebe nada
+        //função trim retira os espaços em branco no inicio e fim da string
+    } else if(!isNaN(Name.value)) {
+        alert("It's a number! Type words")
 
-     alert("Nome = " + NameValue + "Idade = " + AgeValue + "Genero = " + GenderValue) 
+        //isNaN = is not a number
+        // !isNaN() - verifica se é um número
+        // isNaN() - verifica se não é um número
+    }
+
+    if(Age.value > 110) {
+        alert("Age is too high, try less! ")
+    }
+
+    if(Gender[0].checked) {
+        SelectedGender = "Male"
+    } else if(Gender[1].checked) {
+        SelectedGender = "Female"
+    }
+
 }
+
+
+// Criando elementos
+/* 
+var img = document.createElement('img') criei uma tag img <img src = "">
+
+img.setAttribute('id', "foto") criei um id com o valor foto 
+
+<img id = "foto" src = "">
+
+img.setAttribute('src', 'diretorio da imagem') cria o src com o direitorio
+
+e uma forma para colocar a imagem no html 
+
+div.appendChild(img)  appendChild adicionar um elemento filho sendo a div o pai
+ */
